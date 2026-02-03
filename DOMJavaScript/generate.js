63e5c2f241e2d7c187f6d67e5efdb32de013a664
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-02-03 21:12:46 +0700

    Menambahkan File Generate DOM

---
+++ DOMJavaScript/generate.js
@@ -0,0 +1,15 @@
+const button = document.querySelector(`button`);
+const h1 = document.querySelector(`h1`);
+
+button.addEventListener(`click`, () => {
+  const newColor = generateRandomColor();
+  console.log(newColor);
+});
+
+const generateRandomColor = () => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+
+  return `rgb (${r},${g},${b})`;
+};

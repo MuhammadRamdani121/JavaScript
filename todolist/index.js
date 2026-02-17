e010f759b67dd3afb6beda45f5d8c91d9a7b8b68
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-02-17 23:14:15 +0700

    Menambahkan Soal JavaScript

---
+++ todolist/index.js
@@ -0,0 +1,13 @@
+// const textTodo = document.getElementById("text-todo");
+// const tambahTodo = document.getElementById("tambah-todo");
+// const hapusTodo = document.getElementById("button-hapus");
+
+// if (hapusTodo) {
+// }
+
+const mobilAngkotNgetem = 10;
+const mobilAngkotPenuh = 5;
+
+for (let i = 0; i < mobilAngkotNgetem; i++) {
+  console.log("angkot " + i);
+}
